9ec7b0d3a4f1b5052fecea82f3b550469bacdc80
leonardo-kimura <leo.kimura07@gmail.com>
2025-10-19 11:28:02 -0300

    .

---
+++ Ex_módulos/Ex3/main.py
@@ -0,0 +1,12 @@
+from texto_utils import *
+
+texto = input("Digite o texto: ")
+qtd_vogais = contar_vogais(texto)
+print(f"O texto tem {qtd_vogais} vogais.")
+
+qtd_palavras = contar_palavras(texto)
+print(f"O texto tem {qtd_palavras} palavras.")
+
+texto_invertido = inverter_texto(texto)
+print(f"O texto invertido: {texto_invertido}")
+
---
+++ Ex_módulos/Ex3/texto_utils.py
@@ -0,0 +1,17 @@
+def contar_vogais(texto: str) -> int:
+    vogais_validas = "aeiou"
+    cont = 0
+    for letra in texto:
+        if letra.lower() in vogais_validas:
+            cont+=1
+    return cont
+
+def contar_palavras(texto):
+    palavras = texto.split()
+    numero_palavras = len(palavras)
+    return numero_palavras
+
+def inverter_texto(texto):
+    texto_invertido = texto[::-1]
+    return texto_invertido
+
